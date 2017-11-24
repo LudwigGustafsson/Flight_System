9a631343cafed8c512a55f0eb80d2d992366ac96
LudwigGustafsson <32685433+LudwigGustafsson@users.noreply.github.com>
2017-11-24 14:00:50 +0100

    Update Constants.java

--- src/Flight_System/Constants.java
+++ src/Flight_System/Constants.java
@@ -7,8 +7,8 @@ package Flight_System;
 public class Constants
 {
 	public static final int SEATSFOREVERYCLASS = 5;
-	public static final int FÖRSTAKLASSPRIS = 20_000;
-	public static final int EKONOMIKLASSPRIS = 5000;
+	public static final int FIRSTCLASSPRICE = 20_000;
+	public static final int ECONOMYCLASSPRICE = 5000;
 	public static final int SCREEN_WIDTH = 1920;
 	public static final int SCREEN_HEIGHT = 1080;
 	public static final int SPEED = 40;
